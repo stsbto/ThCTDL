2c89e52cb08204d539b1968a94121135b1729a5c
stsbto <stsbto@gmail.com>
2022-05-31 10:40:59 +0700

    add file

---
+++ list.h
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+class List
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void push_back(int data)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            n->prev = tail;
+            tail = n;
+        }
+    }
+    void printList()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+    
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "list.h"
+int main()
+{
+    List l;
+    l.push_back(5);
+    l.push_back(4);
+    l.push_back(6);
+    l.push_back(1);
+    l.push_back(7);
+    l.push_back(3);
+    l.push_back(2);
+    l.printList();
+    return 0;
+}
