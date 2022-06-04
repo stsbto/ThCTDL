ff668cf9a13b00ae505d13440b35da4faa1a5b9c
stsbto <stsbto@gmail.com>
2022-06-04 12:07:42 +0700

    custom main

--- main.cpp
+++ main.cpp
@@ -5,14 +5,11 @@ int main()
     List l;
     readFile(l);
     l.printList();
-    // l.printList();
-    // l.insertionSort();
     // l.selectionSort();
     // l.insertionSort();
     // l.quickSort(l.getHead(), l.getTail());
     // l.shellSort();
     l.radixSort();
-    // l.MergeSort(l.getHead());
     l.printList();
     return 0;
 }
