3f4fd547f3141139f276f7d7b51a19147c67ce3d
stsbto <stsbto@gmail.com>
2022-06-05 11:52:56 +0700

    sorting

---
+++ Sorting/02_insertionSort.cpp
@@ -0,0 +1,125 @@
+// interchangeSort selectionSort insertionSort quickSort shellSort radixSort shakerSort
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+class List
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void push_back(int data)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            n->prev = tail;
+            tail = n;
+        }
+    }
+    Node *getHead()
+    {
+        return head;
+    }
+    Node *getTail()
+    {
+        return tail;
+    }
+    void printList()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    void swapNode(Node *a, Node *b)
+    {
+        int data = a->data;
+        a->data = b->data;
+        b->data = data;
+    }
+    void insertionSort()
+    {
+        Node *pos = head->next;
+        while (pos)
+        {
+            Node *sorted = pos->prev;
+            int data = pos->data;
+            while (sorted && sorted->data > data)
+            {
+                sorted->next->data = sorted->data;
+                sorted = sorted->prev;
+            }
+            if (sorted)
+            {
+                sorted->next->data = data;
+            }
+            else
+            {
+                head->data = data;
+            }
+            pos = pos->next;
+        }
+    }
+};
+void readFile(List &l)
+{
+    cout << "Da doc file txt: " << endl;
+    int data;
+    ifstream myfile("./input.txt");
+    while (!myfile.eof())
+    {
+        myfile >> data;
+        l.push_back(data);
+    }
+    myfile.close();
+}
+void writeFile(List &l)
+{
+    ofstream myfile("output.txt");
+    Node *temp = l.getHead();
+    while (temp)
+    {
+        myfile << temp->data << " ";
+        temp = temp->next;
+    }
+    myfile.close();
+}
+int main()
+{
+    List l;
+    readFile(l);
+    l.printList();
+    l.insertionSort();
+    cout << "insertionSort: " << endl;
+    l.printList();
+    return 0;
+}
